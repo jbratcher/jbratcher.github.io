1e82bbb61f8905d30cb8b6dee2cd9ba6e911f042
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-12-11 13:57:42 +0000

    added floating bubble animation in contact section, change opacity of nav on scroll

--- src/js/wobbleup.js
+++ src/js/wobbleup.js
@@ -4,9 +4,9 @@
 
 console.log("js connected");
 
-// Variables
+// Variables 
 
-var sectionHeight = 600;
+const sectionHeight = 620;
 
 // Set up the canvas and make full screen
 
@@ -41,21 +41,30 @@ window.addEventListener("resize", function() {
     canvas.width = window.innerWidth;
     canvas.height = sectionHeight;
     init();
-})
+});
+
+// Utility Functions
+
+function randomIntFromRange(min, max) {
+    return Math.random() * (max - min + 1) + min;
+}
 
 // Create Circle function
 
-function Circle(x,y,dx,dy,rad,color) {
+function Circle(x,y,dx,dy,rad,color, boundaryRight, boundaryLeft) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.rad = rad;
+    this.boundaryRight = boundaryRight;
+    this.boundaryLeft = boundaryLeft;
     var minRad = rad;
     var maxRad = (rad * 2);
     this.color = color;
     
     // Draw circle function
+    
     this.draw = function() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.rad, 0, Math.PI *2, false);
@@ -64,26 +73,37 @@ function Circle(x,y,dx,dy,rad,color) {
     };
     
     // Update circle position for animation
+    
     this.update = function() {
         
-        // Move circle to bottom once it reaches top
+        // Move circle to top once it reaches bottom
         
-        if(this.y + this.rad > window.innerHeight) {
-            this.y = 0;
+        if(this.y + this.rad < 0) {
+            this.y = sectionHeight;
+        }
+        
+        if(this.x > boundaryRight || this.x < boundaryLeft) {
+            this.dx = -this.dx;
         }
         
         // Increment position (x,y)
+        
         this.x += this.dx;
         this.y += this.dy;
         
         // Interactivity (mouse and circles)
+        
         // Mouse detection for circle
+        
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+            
             // Limit circle grow size
             if (this.rad < maxRad) {
                 this.rad += 1;   
             }
-        // Limit circle shrink size    
+            
+        // Limit circle shrink size  
+        
         } else if (this.rad > minRad) {
             this.rad -= 1;
         } else if (this.rad < this.minRad) {
@@ -91,46 +111,62 @@ function Circle(x,y,dx,dy,rad,color) {
         }
         
         // Draw Circle
+        
         this.draw();
         
     };
+
 }
 
 // Create circles array
+
 var circles = [];
 
 function init() {
     
     // Reset circles array
+    
     circles = [];
     
     // Randomize circle value (position, velocity, fill and stroke color, and opacity)
+    
     for (var i = 0; i < 100; i++) {
-        var rad = (Math.floor(Math.random() * 4)) + 1;
-        var x = Math.random() * (window.innerWidth - rad * 2) + rad;
-        var y = window.innerHeight - (rad *2);
-        var dx = 0;
-        var dy = (Math.random() - 0.5) * 3;
+        var rad = randomIntFromRange(2,4);
+        var x = Math.random() * (window.innerWidth - rad * 2);
+        var y = Math.random() * (window.innerHeight - rad * 2);
+        var dx = 0.2;
+        var dy = -randomIntFromRange(0.2,0.3);
         var color = "white";
-        circles.push(new Circle(x,y,dx,dy,rad,color));
-        
-        console.log(circles);
+        var boundaryRight = x + rad;
+        var boundaryLeft = x - rad;
+        console.log(dy);
+        circles.push(new Circle(x,y,dx,dy,rad,color,boundaryRight,boundaryLeft));
     }
+    
+
 }
 
 //  Animation function
+
 function animation() {
+    
     //Start loop
+    
     requestAnimationFrame(animation);
+    
     // Clear window after drawing circle
+    
     ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+    
     // Draw the circles
+    
     for (var i = 0; i < circles.length; i++) {
         circles[i].update();
     }
     
 }
 
-// Run 
+// Run
+
 animation();
 init();
