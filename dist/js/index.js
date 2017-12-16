4a0e25edbeca695de9f23eecc8cc4d0283fbfe11
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-12-16 18:42:08 +0000

    added some schema markup and updated gulpfile

---
+++ dist/js/index.js
@@ -0,0 +1,15 @@
+var body = document.querySelector("body");
+var header = document.querySelector("header")
+
+var full = function() {
+    
+    document.querySelector("header").style.opacity = 1;
+    
+}
+
+body.onscroll = function() {
+
+    document.querySelector("header").style.opacity = 0.3;
+        
+    setTimeout(full, 2000);
+}
