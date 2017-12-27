a70a4a764f0a3402b14cef7f5e22d5ef1efde7e2
Jeremy Bratcher <jeremybratcher@gmail.com>
2017-12-27 00:59:51 +0000

    updated index.js to es6, added custom variables, documenting

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,47 @@
-var body = document.querySelector("body");
-var header = document.querySelector("header");
+//Variables
 
-var navOpacity = function() {
+// DOM body and header
+
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+
+// The area above the fold of the site
+
+let areaAboveFold = 1200;
+
+// Custom ratios
+
+let full = 1;
+let med = 0.6;
+let low = 0.3;
+let none = 0;
+
+// Set header opacity to 1 for prominent navbar
+
+let navOpacity = function() {
     
-    header.style.opacity = 1;
+    header.style.opacity = full;
     
 };
 
+//  When page scrolls make header transparent, disappear below the fold, and reappear above the fold
+
 body.onscroll = function() {
     
-    if (window.pageYOffset <= 1200) {
+    if (window.pageYOffset <= areaAboveFold) {
         
-        header.style.opacity = 0.3;
+        header.style.opacity = low;
         header.style.display = "flex";
     
-    } else if (window.pageYOffset > 1200) {
+    } else if (window.pageYOffset > areaAboveFold) {
         
         header.style.display = "none";
         
     }
     
-    setTimeout(navOpacity, 2000);
+    // Set a  delay of x seconds for navbar changes
+    
+    setTimeout(navOpacity, 1000);
 
 };
 
