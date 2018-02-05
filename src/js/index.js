88598e781926baee9d779b2e213d2e594f38bb47
Jeremy Bratcher <jeremybratcher@gmail.com>
2018-02-05 12:28:31 +0000

    css refactor

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,17 @@
-//Variables
-
-// DOM body and header
-
 const body = document.querySelector("body");
 const header = document.querySelector("header");
 
-// The area above the fold of the site
-
 let areaAboveFold = 1200;
 
-// Custom ratios
+// Custom ratios for opactiy
 
 let full = 1;
-let med = 0.6;
 let low = 0.3;
-let none = 0;
 
-// Set header opacity to 1 for prominent navbar
+// Set header opacity to 1
 
 let navOpacity = function() {
-    
     header.style.opacity = full;
-    
 };
 
 //  When page scrolls make header transparent, disappear below the fold, and reappear above the fold
@@ -29,14 +19,10 @@ let navOpacity = function() {
 body.onscroll = function() {
     
     if (window.pageYOffset <= areaAboveFold) {
-        
         header.style.opacity = low;
         header.style.display = "flex";
-    
     } else if (window.pageYOffset > areaAboveFold) {
-        
         header.style.display = "none";
-        
     }
     
     // Set a  delay of x seconds for navbar changes
