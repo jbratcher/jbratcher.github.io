98faf683f5ee8c45b672f74028f3e5d1438e9ca5
Jeremy Bratcher <jeremybratcher@gmail.com>
2018-05-20 15:33:42 +0000

    removed init() function from html and placed in index.js, nested index.js functionality inside init function and called at end of file

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,38 @@
-const body = document.querySelector("body");
-const header = document.querySelector("header");
-
-let areaAboveFold = 1200;
-
-// Custom ratios for opactiy
-
-let full = 1;
-let low = 0.3;
-
-// Set header opacity to 1
-
-let navOpacity = () => {
-    header.style.opacity = full;
-};
-
-//  When page scrolls, make header transparent, disappear below the fold, and reappear above the fold
-
-body.onscroll = () => {
+function init() {
     
-    if (window.pageYOffset <= areaAboveFold) {
-        header.style.opacity = low;
-        header.style.display = "flex";
-    } else if (window.pageYOffset > areaAboveFold) {
-        header.style.display = "none";
-    }
+    const body = document.querySelector("body");
+    const header = document.querySelector("header");
     
-    // Set a  delay of x seconds for navbar changes
+    let areaAboveFold = 1200;
     
-    setTimeout(navOpacity, 1000);
+    // Custom ratios for opactiy
+    
+    let full = 1;
+    let low = 0.3;
+    
+    // Set header opacity to 1
+    
+    let navOpacity = () => {
+        header.style.opacity = full;
+    };
+    
+    //  When page scrolls, make header transparent, disappear below the fold, and reappear above the fold
+    
+    body.onscroll = () => {
+        
+        if (window.pageYOffset <= areaAboveFold) {
+            header.style.opacity = low;
+            header.style.display = "flex";
+        } else if (window.pageYOffset > areaAboveFold) {
+            header.style.display = "none";
+        }
+        
+        // Set a  delay of x seconds for navbar changes
+        
+        setTimeout(navOpacity, 1000);
+    
+    };
+    
+}
 
-};
-
+init();
