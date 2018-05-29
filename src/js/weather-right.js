f00b24d2dcf605a4bb9df043ce439256790eecfd
jbratcher <jeremybratcher@gmail.com>
2018-05-29 19:30:37 -0400

    changed canvasPort background to radial

--- src/js/weather-right.js
+++ src/js/weather-right.js
@@ -60,56 +60,56 @@ function Circle(x,y,dx,dy,rad,color) {
     var minRad = rad;
     var maxRad = (rad * 2);
     this.color = color;
-    
+
     // Draw circle function
-    
+
     this.draw = () => {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.rad, 0, Math.PI *2, false);
         ctx.fillStyle = this.color;
         ctx.fill();
     };
-    
+
     // Update circle position for animation
     this.update = function() {
-        
+
         // Loop circle when it hits the side of the window
-        
+
         if(this.x + this.rad > elementWidth) {
             this.x = 0;
         }
-        
+
         if(this.y + this.rad > elementHeight) {
             this.y = 0;
         }
-        
+
         // Increment position (x,y)
-        
+
         this.x += this.dx;
         this.y += this.dy;
-        
+
         // Interactivity (mouse and circles)
-        
+
         // Mouse detection for circle
-        
+
         if (mouse.x - this.x < 1 && mouse.x - this.x > -1 && mouse.y - this.y < 1 && mouse.y - this.y > -1) {
             // Limit circle grow size
             if (this.rad < maxRad) {
-                this.rad += 1;   
+                this.rad += 1;
             }
-            
-        // Limit circle shrink size    
-        
+
+        // Limit circle shrink size
+
         } else if (this.rad > minRad) {
             this.rad -= 1;
         } else if (this.rad < this.minRad) {
             this.rad += 1;
         }
-        
+
         // Draw Circle
-        
+
         this.draw();
-        
+
     };
 }
 
@@ -118,13 +118,13 @@ function Circle(x,y,dx,dy,rad,color) {
 let circles = [];
 
 function init() {
-    
+
     // Reset circles array
-    
+
     circles = [];
-    
+
     // Randomize circle value (position, velocity, fill and stroke color, and opacity)
-    
+
     for (let i = 0; i < 100; i++) {
         let rad = randomIntFromRange(2,4);
         let x = Math.random() * (elementWidth - rad * 2) + rad;
@@ -133,31 +133,31 @@ function init() {
         let dy = randomIntFromRange(0.5,1);
         let color = "white";
         circles.push(new Circle(x,y,dx,dy,rad,color));
-        
+
     }
 }
 
 //  Animation function
 
 const animation = () => {
-    
+
     // Start animation loop
-    
+
     requestAnimationFrame(animation);
-    
+
     // Clear window after drawing circle
-    
+
     ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
-    
+
     // Draw the circles
-    
+
     for (var i = 0; i < circles.length; i++) {
         circles[i].update();
     }
-    
+
 };
 
-// Run 
+// Run
 
 animation();
 init();
